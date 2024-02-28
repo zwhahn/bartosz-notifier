ecffafc1153ed5f9d2609d2f2751544c543a8690
zwhahn <117476820+zwhahn@users.noreply.github.com>
2024-02-27 20:50:58 -0800

    Import email_inputs and smtplib
    Create send_email function

---
+++ main.py
@@ -0,0 +1,11 @@
+import smtplib
+import email_inputs
+
+#  username, password
+
+username = email_inputs.username
+password = email_inputs.password
+
+def send_email():
+    sender_email = 'user@example.com'
+    receiver_email = 'user@example.com'
