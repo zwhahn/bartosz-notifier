ac285a46154f4f54f5067188fd8e239a506b6a01
zwhahn <117476820+zwhahn@users.noreply.github.com>
2024-02-28 19:26:26 -0800

    Correct TO_EMAIL and change from ' to "
    Succesful email sent

--- main.py
+++ main.py
@@ -5,14 +5,13 @@ HOST = 'smtp-mail.outlook.com'  # SMTP server domain of outlook
 PORT = 587
 
 FROM_EMAIL = email_inputs.email_adress
-TO_EMAIL = 'mail_inputs.email_adress'
+TO_EMAIL = email_inputs.email_adress
 PASSWORD = email_inputs.password
 
-MESSAGE = '''Subject: Python script email
-Hello World
-'''
+MESSAGE = """Subject: Python script email
+Hello World"""
 
-smtp = smtplib.SMTP_SSL(HOST, PORT)
+smtp = smtplib.SMTP(HOST, PORT)
 
 status_code, response = smtp.ehlo()  # ping server and check if it's running
 print(f"Echoing the server: {status_code}, {response}")
